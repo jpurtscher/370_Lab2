b545144ba482a5d58e38dddde0a921b337f57a26
Joshua Purtscher <jpurtscher28@gmail.com>
2026-09-18 12:51:38 -0700

    Update HelloApplication and ignore target folder

--- src/main/java/com/example/graph_app2/HelloApplication.java
+++ src/main/java/com/example/graph_app2/HelloApplication.java
@@ -72,4 +72,8 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
